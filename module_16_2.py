92b492598b815df03f7cc951450560bc13cfefbc
astakhova-kv <prisakarkv@gmail.com>
2024-12-27 14:57:34 +0700

    Update module_16_2.py

--- module_16_2.py
+++ module_16_2.py
@@ -15,11 +15,11 @@ async def adm() -> dict:
 
 
 @app.get('/user/{user_id}')
-async def user_ids(user_id: Annotated[int, Path(min_length=1, max_length=100, description='Enter User ID', example='1')]) -> dict:
+async def user_ids(user_id: Annotated[int, Path(ge=1, le=100, description='Enter User ID', example='1')]) -> dict:
     return {'message': f'Вы вошли как пользователь № {user_id}'}
 
 
 @app.get('/user/{username}/{age}')
 async def users(username: Annotated[str, Path(min_length=5, max_length=20, description='Enter username', example='UrbanUser')],
-                age: Annotated[int, Path(min_length=18, max_length=120, description='Enter age', example='24')]) -> dict:
+                age: Annotated[int, Path(ge=18, le=120, description='Enter age', example='24')]) -> dict:
     return {'message': f'Информация о пользователе. Имя: {username}, Возраст: {age}'}
