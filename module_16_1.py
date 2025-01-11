409239780794f48fa461fd9e4216f95200a6bc56
astakhova-kv <prisakarkv@gmail.com>
2025-01-11 08:37:19 +0700

    Update module_16_1.py

--- module_16_1.py
+++ module_16_1.py
@@ -4,20 +4,20 @@ app = FastAPI()
 
 
 @app.get('/')
-async def welcome() -> dict:
-    return {'message': 'Главная страница'}
+async def welcome() -> str:
+    return 'Главная страница'
 
 
 @app.get('/user/admin')
-async def adm() -> dict:
-    return {'message': 'Вы вошли как администратор'}
+async def adm() -> str:
+    return 'Вы вошли как администратор'
 
 
 @app.get('/user/{user_id}')
-async def user_ids(user_id: int = 1) -> dict:
-    return {'message': f'Вы вошли как пользователь № {user_id}'}
+async def user_ids(user_id: int = 1) -> str:
+    return f'Вы вошли как пользователь № {user_id}'
 
 
 @app.get('/user')
-async def users(username: str = 'Kris', age: int = 36) -> dict:
-    return {'message': f'Информация о пользователе. Имя: {username}, Возраст: {age}'}
+async def users(username: str = 'Kris', age: int = 36) -> str:
+    return f'Информация о пользователе. Имя: {username}, Возраст: {age}'
